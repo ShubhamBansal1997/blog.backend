057c3c0333a83a2cee0ea718d15e93558434b0b5
Shubham Bansal <shubhambansal17@hotmail.com>
2019-06-30 02:08:12 +0530

    [build]: Inital Commit
    - Github Issue:
    
    Authored-by: Shubham Bansal <shubhambansal17@hotmail.com>
    Signed-off-by: Shubham Bansal <shubhambansal17@hotmail.com>

---
+++ app/Http/Requests/RegisterAuthRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
+
+class RegisterAuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6|max:10'
+        ];
+    }
+    /**
+     * Custom Failed Response
+     *
+     * Overrides the Illuminate\Foundation\Http\FormRequest
+     * response function to stop it from auto redirecting
+     * and applies a API custom response format.
+     *
+     * @param array $errors
+     * @return JsonResponse
+     */
+    public function response(array $errors) {
+
+        // Put whatever response you want here.
+        return new JsonResponse([
+            'status' => '422',
+            'errors' => $errors,
+        ], 422);
+    }
+}
